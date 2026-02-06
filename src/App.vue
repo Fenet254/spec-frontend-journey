00f11ac9ae840ab207175c593a7fb8c7298251c9
Fenet Gizaw <simbani011@gmail.com>
2026-02-06 06:14:55 -0800

    exersise of event handling

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,13 @@
-<!-- <template>
-  <div class="container">
-
-
-    <input
-      type="text"
-      v-model="name"
-      placeholder="Enter your name"
-    />
-
-    
-    <p v-if="name.trim().length > 0">
-      Hi there, {{ name }} 
-    </p>
-  </div>
-</template>
-
-<script setup>
-import { ref } from "vue";
-
-const name = ref("");
-</script>
-
-<style>
-.container {
-  padding: 20px;
-  font-family: Arial, sans-serif;
-}
-
-input {
-  padding: 8px;
-  margin-top: 10px;
-  width: 250px;
-}
-
-p {
-  margin-top: 15px;
-  font-size: 40px;
-  color: rgb(224, 220, 228);
-}
-</style>
--->
 <template>
-  <div class="page">
+  <div>Hello world</div>
 
-
-    <div class="card-list">
-      <UserCard name="abebe" role=" role1" />
-      <UserCard name="kebede" role="role2" />
-      <UserCard name="chaltu" role="role3" />
-    </div>
-  </div>
+  <counter></counter>
+  <TemplateSyntax />
+  <ComputedComponent />
+  <ClassComponent class="foo bar" />
+  <StyleComponent class="foo bar" />
+  <ConditionalComponent class="foo bar" />
+  <ListRendering class="foo bar" />
+  <EventHandling class="foo bar" />
+  <div style="margin-top: 15rem"></div>
 </template>
-
-<script setup>
-import UserCard from "./components/UserCard.vue"
-</script>
-
-<style scoped>
-.page {
-  padding: 20px;
-  font-family: Arial, sans-serif;
-}
-
-.card-list {
-  display: flex;
-  gap: 20px;
-  margin-top: 15px;
-}
-</style>
-
---
+++ src/components/EventHandling.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h3>Event Handling</h3>
+
+    <div>
+      <button @click="$event => incrementCounter('leela', $event)">Counter</button>
+      <div>Counter: {{ count }}</div>
+    </div>
+
+    <div @click.capture="onParentClick">
+      <a href="http://www.google.com" @click.stop.prevent="onLinkClick">Click here</a>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'; // 158.6k (gzipped: 58.3k)
+
+const count = ref(0);
+
+const incrementCounter = (name, event) => {
+  count.value++;
+  console.log(name);
+  console.log(event.target.tagName);
+};
+
+const onLinkClick = () => {
+  console.log('link Clicked');
+};
+
+const onParentClick = () => {
+  console.log('parent Clicked');
+};
+</script>
