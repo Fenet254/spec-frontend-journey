33588a85b15ce29083f3c574229b5ce54509a0cc
Fenet Gizaw <simbani011@gmail.com>
2026-02-06 10:22:03 -0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
-import './assets/main.css'
+import { createApp } from 'vue'; // 158.7k (gzipped: 58.3k)
+import App from './App.vue';
+import Counter from './components/Counter.vue';
+import TemplateSyntax from './components/TemplateSyntax.vue';
+import ComputedComponent from './components/ComputedComponent.vue';
+import ClassComponent from './components/ClassComponent.vue';
+import StyleComponent from './components/StyleComponent.vue';
+import ConditionalComponent from './components/ConditionalComponent.vue';
+import ListRendering from './components/ListRendering.vue';
+import EventHandling from './components/EventHandling.vue';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+const app = createApp(App);
 
-createApp(App).mount('#app')
+app.component('Counter', Counter);
+app.component('TemplateSyntax', TemplateSyntax);
+app.component('ComputedComponent', ComputedComponent);
+app.component('ClassComponent', ClassComponent);
+app.component('StyleComponent', StyleComponent);
+app.component('ConditionalComponent', ConditionalComponent);
+app.component('ListRendering', ListRendering);
+app.component('EventHandling', EventHandling);
+
+app.mount('#app');
