8d505cfae8b510f5accdf286e16560101113e68f
Fenet Gizaw <simbani011@gmail.com>
2026-02-26 12:51:18 -0800

    Update app.vue

--- spec-frontend-journey/app/app.vue
+++ spec-frontend-journey/app/app.vue
@@ -1,6 +1,14 @@
-<template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
-</template>
+import { createApp ,  ref } from 'vue '
+createApp({
+  setup(){
+    return {
+ count:ref(0)
+    }
+  }
+}).mount(]app)
+
+<div id="app">
+<button @onclick = "count++">
+cloick is :{{count}}
+</button>
+</div>
